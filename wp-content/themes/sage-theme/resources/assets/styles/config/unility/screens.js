0e890e96ddac5f41365b986177e1f7064bf864d5
Long97 <nguyenthanhlong971008@gmail.com>
2024-04-23 16:43:42 +0700

    Fixed module home banner

--- wp-content/themes/sage-theme/resources/assets/styles/config/unility/screens.js
+++ wp-content/themes/sage-theme/resources/assets/styles/config/unility/screens.js
@@ -2,12 +2,13 @@ const Screens = {
   'sm': '481px',
   'md': '768px',
   'lg': '992px',
-  'xl': '1200px',
+  'xl': '1024px',
   '2xl': '1440px',
   '2k': '2000px',
-  'down_xl': {'max': '1199px'},
+  'down_2xl': {'max': '1440px'},
+  'down_xl': {'max': '1024px'},
   'down_lg': {'max': '991px'},
-  'down_md': {'max': '767px'},
+  'down_md': {'max': '768px'},
   'down_sm': {'max': '480px'}
 }
 module.exports = {
---
+++ wp-content/uploads/cache/9f6bef38b0550b1a882473faa7769eb5483ca964.php
@@ -0,0 +1,51 @@
+<section class="mod-section2">
+    <div class="container">
+        <div class=" flex justify-center flex-wrap">
+            <div class="main-title">
+                <div class="line"></div>
+                <h2>
+                    The Encompass Platform Seamlessly Connects the Supply Chain, Allowing You to:
+                </h2>
+            </div>
+            <div class="content-list">
+                <div class="content-item">
+                    <h3>Drive Business Growth</h3>
+                    <div class="content-desc">
+                        <p>Tap into the power of predictive analytics and demand forecasting. Capitalize on
+                            revenue-generating opportunities faster. Encompass ERP captures mission-critical data across your
+                            entire operation, empowering you to bring your beverage or food business into new levels of
+                            profitability.
+                        </p>
+                    </div>
+                </div>
+                <div class="content-item">
+                    <h3>Leverage Real-Time Data for Better Decision-Making</h3>
+                    <div class="content-desc">
+                        <p>Accelerate business decisions with better, more accurate system-to-system data
+                            and comprehensive reporting that results in deep, actionable insights. Stay ahead of potential
+                            disruptions and maintain a finger on the pulse of your supply chain from anywhere in the world.
+                        </p>
+                    </div>
+                </div>
+                <div class="content-item">
+                    <h3>Improve Operational Efficiencies</h3>
+                    <div class="content-desc">
+                        <p>Streamline processes and save money by reducing leakage with a fully integrated
+                            ERP system that works for your business. Encompass ERP makes it possible to identify where, when,
+                            and how resources can be best allocated to provide the most optimal outcome.
+                        </p>
+                    </div>
+                </div>
+                <div class="content-item">
+                    <h3>Enhance Team Dynamics &amp; Employee Satisfaction</h3>
+                    <div class="content-desc">
+                        <p>When your labor force has the tools they need to succeed, the workplace runs like
+                            a well-oiled machine. With Encompass ERP, every stakeholder across your supply chain—from purchasing
+                            to Point-of-Sale—has what they need to do their jobs effectively.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
